56e1b626e2f14448451723bfc975a9caa1037bec
KouheiSakamoto <sakamotokouhei@sakamotokouheinoMacBook-Pro.local>
2019-12-30 21:16:53 +0900

    index.php を追加

---
+++ src/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Brothers</title>
+<link rel ="stylesheet" type="text/css" href ="br_index.css">
+
+
+
+</head>
+
+<body>
+<div class="header">
+
+<h1>Brothers Official site</h1>
+<h2>気ままな仲間たちのダラダラ生活</h2>
+<ul>
+<li>ブラザーズとは</li>
+<li>連盟加盟員</li>
+<li>作品コーナー</li>
+</ul>
+
+
+</body>
+
+
+
+
+
+
+
+
+
+
+</html>
