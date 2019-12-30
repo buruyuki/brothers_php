591da6baa63732bc902407a2a311f149c6a8e294
buruyuki <buruaceyuki@gmail.com>
2019-12-30 22:55:29 +0900

    slider add

--- src/index.php
+++ src/index.php
@@ -3,9 +3,12 @@
 <head>
   <meta charset="utf-8">
   <title>Brothers</title>
-<link rel ="stylesheet" type="text/css" href ="br_index.css">
+<link rel ="stylesheet" href="resources/styles.css">
 
-
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/jquery.slick/1.6.0/slick.css"/>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/jquery.slick/1.6.0/slick-theme.css">
+<script src="https://code.jquery.com/jquery-2.2.0.min.js"></script>
+<script src="https://cdn.jsdelivr.net/jquery.slick/1.6.0/slick.min.js"></script>
 
 </head>
 
@@ -13,7 +16,33 @@
 <div class="header">
 
 <h1>Brothers Official site</h1>
+
+<div class="slider">
+<!--スライダーのコンテンツ（画像）を記述-->
+  <section class="regular slider">
+    <div>
+      <img src="https://avatars1.githubusercontent.com/u/51830305?s=460&v=4">
+      ぶる
+    </div>
+    <div>
+      <img src="https://avatars1.githubusercontent.com/u/59357198?s=460&v=4">
+      ほそ
+    </div>
+  </section>
+</div>
+<!--スライダーのレイアウト・動作設定を記述-->
+  <script type="text/javascript">
+    $(document).on('ready', function() {
+      $(".regular").slick({
+        dots: true,
+        autoplay: true,//自動再生あり
+        autoplaySpeed: 2000,//自動再生でのスライダー切替スピード2000ms(2秒)
+      });
+    });
+  </script>
+
 <h2>気ままな仲間たちのダラダラ生活</h2>
+
 <ul>
 <li>ブラザーズとは</li>
 <li>連盟加盟員</li>
@@ -31,6 +60,6 @@
 
 
 
-
+<a href="https://github.com/buruyuki/brothers">GitHubはこちら &gt;&gt;</a>
 
 </html>
